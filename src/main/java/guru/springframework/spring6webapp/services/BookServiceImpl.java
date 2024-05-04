07a3962c9744da531b20ac017f6b0fef12b6317f
ivanxalie <ivanxalie@gmail.com>
2024-05-04 23:44:14 +0300

    Add BookService

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository repository;
+
+    public BookServiceImpl(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return repository.findAll();
+    }
+}
